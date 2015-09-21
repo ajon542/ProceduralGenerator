bd36b202a494239e5351a4b4a6ccea573b6511d2
Andrew Jones <ajon542@gmail.com>
2015-09-21 12:31:23 -0700

    Added voxel support based on the tutorial at http://catlikecoding.com/unity/tutorials/marching-squares/

---
+++ ProceduralGenerator/Assets/Scripts/VoxelGrid.cs
@@ -0,0 +1,103 @@
+﻿namespace MapBuilder
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Component to contain the voxels.
+    /// </summary>
+    [SelectionBase]
+    public class VoxelGrid : MonoBehaviour
+    {
+        /// <summary>
+        /// Resolution of the grid.
+        /// </summary>
+        /// <example>
+        /// For an 8x8 grid, set the resolution to 8.
+        /// </example>
+        public int resolution;
+
+        /// <summary>
+        /// Voxel prefab.
+        /// </summary>
+        public GameObject voxelPrefab;
+
+        /// <summary>
+        /// Voxel state.
+        /// </summary>
+        private bool[] voxels;
+
+        /// <summary>
+        /// Size of the individual voxels.
+        /// </summary>
+        private float voxelSize;
+
+        /// <summary>
+        /// Individual voxel materials for setting the colour based on the voxel state.
+        /// </summary>
+        private Material[] voxelMaterials;
+
+        /// <summary>
+        /// Initialize the voxel grid.
+        /// </summary>
+        /// <param name="resolution">The resolution of the grid.</param>
+        /// <param name="size">The size of the grid.</param>
+        public void Initialize(int resolution, float size)
+        {
+            // Initialize the grid data.
+            this.resolution = resolution;
+            voxelSize = size / resolution;
+            voxels = new bool[resolution * resolution];
+            voxelMaterials = new Material[voxels.Length];
+
+            // Create the voxel game objects.
+            for (int i = 0, y = 0; y < resolution; y++)
+            {
+                for (int x = 0; x < resolution; x++, i++)
+                {
+                    CreateVoxel(i, x, y);
+                }
+            }
+
+            // Set the colours of the voxels.
+            SetVoxelColors();
+        }
+
+        /// <summary>
+        /// Create a voxel game object.
+        /// </summary>
+        /// <param name="i">The index of the voxel.</param>
+        /// <param name="x">Voxel x-coordinate.</param>
+        /// <param name="y">Voxel y-coordinate.</param>
+        private void CreateVoxel(int i, int x, int y)
+        {
+            GameObject o = Instantiate(voxelPrefab);
+            o.transform.parent = transform;
+            o.transform.localPosition = new Vector3((x + 0.5f) * voxelSize, (y + 0.5f) * voxelSize);
+            o.transform.localScale = Vector3.one * voxelSize * 0.9f;
+            voxelMaterials[i] = o.GetComponent<MeshRenderer>().material;
+        }
+
+        /// <summary>
+        /// Set the voxel state and update their colours.
+        /// </summary>
+        /// <param name="x">Voxel x-coordinate.</param>
+        /// <param name="y">Voxel y-coordinate.</param>
+        /// <param name="state">The new state of the voxel.</param>
+        public void SetVoxel(int x, int y, bool state)
+        {
+            voxels[y * resolution + x] = state;
+            SetVoxelColors();
+        }
+
+        /// <summary>
+        /// Set the colours of the voxels dpeending on their state.
+        /// </summary>
+        private void SetVoxelColors()
+        {
+            for (int i = 0; i < voxels.Length; i++)
+            {
+                voxelMaterials[i].color = voxels[i] ? Color.black : Color.white;
+            }
+        }
+    }
+}
---
+++ ProceduralGenerator/Assets/Scripts/VoxelMap.cs
@@ -0,0 +1,141 @@
+﻿namespace MapBuilder
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Component to manage multiple voxel grids.
+    /// </summary>
+    /// <remarks>
+    /// 
+    /// VoxelMap where a, b, c and d are individual VoxelGrids.
+    /// --------------------
+    /// |        |         |
+    /// |   a    |    b    |
+    /// |________|_________|
+    /// |        |         |
+    /// |   c    |    d    |
+    /// |        |         |
+    /// --------------------
+    /// 
+    /// </remarks>
+    public class VoxelMap : MonoBehaviour
+    {
+        /// <summary>
+        /// Demensions of the voxel map.
+        /// </summary>
+        public float size = 2f;
+
+        /// <summary>
+        /// The voxel grid resolution.
+        /// </summary>
+        public int voxelResolution = 8;
+
+        /// <summary>
+        /// The chunk resolution.
+        /// </summary>
+        public int chunkResolution = 2;
+
+        /// <summary>
+        /// Prefab used for the voxel grids.
+        /// </summary>
+        public VoxelGrid voxelGridPrefab;
+
+        /// <summary>
+        /// Storage for the voxel grids.
+        /// </summary>
+        private VoxelGrid[] chunks;
+
+        /// <summary>
+        /// The size of each chunk.
+        /// </summary>
+        private float chunkSize;
+
+        /// <summary>
+        /// The size of each voxel.
+        /// </summary>
+        private float voxelSize;
+
+        /// <summary>
+        /// The half size of the map.
+        /// </summary>
+        private float halfSize;
+
+        /// <summary>
+        /// Initialize the map.
+        /// </summary>
+        private void Awake()
+        {
+            // Calculate the half size, chunk size and voxel size based on the input.
+            halfSize = size * 0.5f;
+            chunkSize = size / chunkResolution;
+            voxelSize = chunkSize / voxelResolution;
+
+            // Initialize each chunk.
+            chunks = new VoxelGrid[chunkResolution * chunkResolution];
+            for (int i = 0, y = 0; y < chunkResolution; y++)
+            {
+                for (int x = 0; x < chunkResolution; x++, i++)
+                {
+                    CreateChunk(i, x, y);
+                }
+            }
+
+            // Add a collider to detect user interaction.
+            BoxCollider box = gameObject.AddComponent<BoxCollider>();
+            box.size = new Vector3(size, size);
+        }
+
+        /// <summary>
+        /// Create a chunk.
+        /// </summary>
+        /// <param name="i">Chunk index.</param>
+        /// <param name="x">Chunk x-coordinate.</param>
+        /// <param name="y">Chunk y-coordinate.</param>
+        private void CreateChunk(int i, int x, int y)
+        {
+            VoxelGrid chunk = Instantiate(voxelGridPrefab);
+            chunk.Initialize(voxelResolution, chunkSize);
+            chunk.transform.parent = transform;
+            chunk.transform.localPosition = new Vector3(x * chunkSize - halfSize, y * chunkSize - halfSize);
+            chunks[i] = chunk;
+        }
+
+        /// <summary>
+        /// Detect user input and update the voxel map.
+        /// </summary>
+        private void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                RaycastHit hitInfo;
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo))
+                {
+                    if (hitInfo.collider.gameObject == gameObject)
+                    {
+                        EditVoxels(transform.InverseTransformPoint(hitInfo.point));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Update the voxel map.
+        /// </summary>
+        /// <param name="point">The point which was touched.</param>
+        private void EditVoxels(Vector3 point)
+        {
+            // Determine which voxel was touched.
+            int voxelX = (int)((point.x + halfSize) / voxelSize);
+            int voxelY = (int)((point.y + halfSize) / voxelSize);
+            int chunkX = voxelX / voxelResolution;
+            int chunkY = voxelY / voxelResolution;
+            Debug.Log(voxelX + ", " + voxelY + " in chunk " + chunkX + ", " + chunkY);
+
+            voxelX -= chunkX * voxelResolution;
+            voxelY -= chunkY * voxelResolution;
+
+            // Set the state of the voxel.
+            chunks[chunkY * chunkResolution + chunkX].SetVoxel(voxelX, voxelY, true);
+        }
+    }
+}
