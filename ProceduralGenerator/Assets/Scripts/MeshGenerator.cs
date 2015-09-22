bc913807eb37768c0591a1dd612f5cc2c570c2e0
Andrew Jones <ajon542@gmail.com>
2015-09-22 16:03:00 -0700

    Generate the mesh for the cave.

--- ProceduralGenerator/Assets/Scripts/MeshGenerator.cs
+++ ProceduralGenerator/Assets/Scripts/MeshGenerator.cs
@@ -2,20 +2,142 @@
 namespace MapBuilder
 {
     using UnityEngine;
+    using System.Collections.Generic;
     using System.Text;
 
     public class MeshGenerator : MonoBehaviour
     {
         public SquareGrid squareGrid;
 
+        private List<Vector3> vertices;
+        private List<int> triangles;
+
         public void GenerateMesh(int[,] map, float squareSize)
         {
             squareGrid = new SquareGrid(map, squareSize);
+            vertices = new List<Vector3>();
+            triangles = new List<int>();
+
+            for (int x = 0; x < squareGrid.squares.GetLength(0); ++x)
+            {
+                for (int y = 0; y < squareGrid.squares.GetLength(1); ++y)
+                {
+                    TriangulateSquare(squareGrid.squares[x, y]);
+                }
+            }
+
+            Mesh mesh = new Mesh();
+            GetComponent<MeshFilter>().mesh = mesh;
+
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangles.ToArray();
+            mesh.RecalculateNormals();
+        }
+
+        private void TriangulateSquare(Square square)
+        {
+            switch (square.configuration)
+            {
+                case 0:
+                    break;
+
+                case 1:
+                    MeshFromPoints(square.centerBottom, square.bottomLeft, square.centerLeft);
+                    break;
+                case 2:
+                    MeshFromPoints(square.centerRight, square.bottomRight, square.centerBottom);
+                    break;
+                case 4:
+                    MeshFromPoints(square.centerTop, square.topRight, square.centerRight);
+                    break;
+                case 8:
+                    MeshFromPoints(square.topLeft, square.centerTop, square.centerLeft);
+                    break;
+
+                case 3:
+                    MeshFromPoints(square.centerRight, square.bottomRight, square.bottomLeft, square.centerLeft);
+                    break;
+                case 6:
+                    MeshFromPoints(square.centerTop, square.topRight, square.bottomRight, square.centerBottom);
+                    break;
+                case 9:
+                    MeshFromPoints(square.topLeft, square.centerTop, square.centerBottom, square.bottomLeft);
+                    break;
+                case 12:
+                    MeshFromPoints(square.topLeft, square.topRight, square.centerRight, square.centerLeft);
+                    break;
+
+                case 5:
+                    MeshFromPoints(square.centerTop, square.topRight, square.centerRight, square.centerBottom, square.bottomLeft, square.centerLeft);
+                    break;
+                case 10:
+                    MeshFromPoints(square.topLeft, square.centerTop, square.centerRight, square.bottomRight, square.centerBottom, square.centerLeft);
+                    break;
+
+                case 7:
+                    MeshFromPoints(square.centerTop, square.topRight, square.bottomRight, square.bottomLeft, square.centerLeft);
+                    break;
+                case 11:
+                    MeshFromPoints(square.topLeft, square.centerTop, square.centerRight, square.bottomRight, square.bottomLeft);
+                    break;
+                case 13:
+                    MeshFromPoints(square.topLeft, square.topRight, square.centerRight, square.centerBottom, square.bottomLeft);
+                    break;
+                case 14:
+                    MeshFromPoints(square.topLeft, square.topRight, square.bottomRight, square.centerBottom, square.centerLeft);
+                    break;
+
+                case 15:
+                    MeshFromPoints(square.topLeft, square.topRight, square.bottomRight, square.bottomLeft);
+                    break;
+
+            }
+        }
+
+        private void MeshFromPoints(params Node[] points)
+        {
+            AssignVertices(points);
+
+            if (points.Length >= 3)
+            {
+                CreateTriangle(points[0], points[1], points[2]);
+            }
+            if (points.Length >= 4)
+            {
+                CreateTriangle(points[0], points[2], points[3]);
+            }
+            if (points.Length >= 5)
+            {
+                CreateTriangle(points[0], points[3], points[4]);
+            }
+            if (points.Length >= 6)
+            {
+                CreateTriangle(points[0], points[4], points[5]);
+            }
+        }
+
+        private void AssignVertices(Node[] points)
+        {
+            for (int i = 0; i < points.Length; ++i)
+            {
+                if (points[i].vertexIndex == -1)
+                {
+                    points[i].vertexIndex = vertices.Count;
+                    vertices.Add(points[i].position);
+                }
+            }
+        }
+
+        private void CreateTriangle(Node a, Node b, Node c)
+        {
+            triangles.Add(a.vertexIndex);
+            triangles.Add(b.vertexIndex);
+            triangles.Add(c.vertexIndex);
         }
 
         private void OnDrawGizmos()
         {
-            if (squareGrid == null)
+            /*if (squareGrid == null)
             {
                 return;
             }
@@ -42,7 +164,7 @@ namespace MapBuilder
                     Gizmos.DrawCube(squareGrid.squares[x, y].centerLeft.position, Vector3.one * 0.15f);
                     Gizmos.DrawCube(squareGrid.squares[x, y].centerRight.position, Vector3.one * 0.15f);
                 }
-            }
+            }*/
         }
 
         public class Node
@@ -85,6 +207,8 @@ namespace MapBuilder
 
             public Node centerTop, centerRight, centerBottom, centerLeft;
 
+            public int configuration;
+
             public Square(ControlNode topLeft, ControlNode topRight, ControlNode bottomRight, ControlNode bottomLeft)
             {
                 this.topLeft = topLeft;
@@ -96,6 +220,23 @@ namespace MapBuilder
                 centerRight = bottomRight.above;
                 centerBottom = bottomLeft.right;
                 centerLeft = bottomLeft.above;
+
+                if (topLeft.active)
+                {
+                    configuration += 8;
+                }
+                if (topRight.active)
+                {
+                    configuration += 4;
+                }
+                if (bottomRight.active)
+                {
+                    configuration += 2;
+                }
+                if (bottomLeft.active)
+                {
+                    configuration += 1;
+                }
             }
         }
 
