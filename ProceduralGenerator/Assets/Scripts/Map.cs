413436ac9dd65d3128c6db59c66e0b99305bc9ba
Andrew Jones <ajon542@gmail.com>
2015-09-23 18:06:58 -0700

    Added a random map along with the map class.

---
+++ ProceduralGenerator/Assets/Scripts/Map.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Dungeon
+{
+    using System;
+
+    /// <summary>
+    /// Container for a location on the map.
+    /// </summary>
+    public class Location
+    {
+        /// <summary>
+        /// The x-coordinate.
+        /// </summary>
+        public readonly int x;
+
+        /// <summary>
+        /// The y-coordinate.
+        /// </summary>
+        public readonly int y;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="Location"/> class.
+        /// </summary>
+        /// <param name="x">The x-coordinate.</param>
+        /// <param name="y">The y-coordinate.</param>
+        public Location(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    /// <summary>
+    /// Container for the map representation.
+    /// </summary>
+    [Serializable]
+    public class Map
+    {
+        /// <summary>
+        /// Gets the width of the map.
+        /// </summary>
+        public int Width { get; private set; }
+
+        /// <summary>
+        /// Gets the height of the map.
+        /// </summary>
+        public int Height { get; private set; }
+
+        /// <summary>
+        /// The map representation.
+        /// </summary>
+        private int[,] map;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="Map"/> class.
+        /// </summary>
+        /// <param name="width">The width of the map.</param>
+        /// <param name="height">The height of the map.</param>
+        public Map(int width, int height)
+        {
+            Width = width;
+            Height = height;
+
+            map = new int[Width, Height];
+        }
+
+        /// <summary>
+        /// Get the value at the given map location.
+        /// </summary>
+        /// <param name="location">The location.</param>
+        /// <returns>The value at the given location; -1 if the location is not within map bounds.</returns>
+        public int GetValueAtLocation(Location location)
+        {
+            // Check if the location is within map bounds.
+            if (!IsValidLocation(location))
+            {
+                return -1;
+            }
+
+            return map[location.x, location.y];
+        }
+
+        /// <summary>
+        /// Set the value at the given map location.
+        /// </summary>
+        /// <param name="location">The location.</param>
+        /// <param name="value">The value to set.</param>
+        public void SetValueAtLocation(Location location, int value)
+        {
+            // Check if the location is within map bounds.
+            if (!IsValidLocation(location))
+            {
+                return;
+            }
+
+            map[location.x, location.y] = value;
+        }
+
+        /// <summary>
+        /// Determines whether the given location is in the map bounds.
+        /// </summary>
+        /// <param name="location">The location to test.</param>
+        /// <returns>true if the locaiton is within map bounds; false otherwise.</returns>
+        public bool IsValidLocation(Location location)
+        {
+            return location.x >= 0 && location.x < Width && location.y >= 0 && location.y < Height;
+        }
+    }
+}
--- ProceduralGenerator/Assets/Scripts/MapGenerator.cs
+++ ProceduralGenerator/Assets/Scripts/MapGenerator.cs
@@ -1,15 +1,125 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿namespace Dungeon
+{
+    using UnityEngine;
+    using System;
 
-public class MapGenerator : MonoBehaviour {
+    /// <summary>
+    /// Map generator for a single level of the dungeon.
+    /// </summary>
+    /// <remarks>
+    /// This class will take care of the map representation. It will
+    /// hand off responsibility to another class to handle the mesh
+    /// generation.
+    /// </remarks>
+    public class MapGenerator : MonoBehaviour
+    {
+        /// <summary>
+        /// Width of the map.
+        /// </summary>
+        public int width = 180;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        /// <summary>
+        /// Height of the map.
+        /// </summary>
+        public int height = 70;
+
+        /// <summary>
+        /// The fill percentage for the map.
+        /// </summary>
+        public int fillPercentage = 45;
+
+        /// <summary>
+        /// The map data structure.
+        /// </summary>
+        private Map map;
+
+        /// <summary>
+        /// Start building the map.
+        /// </summary>
+        private void Start()
+        {
+            BuildMap();
+        }
+
+        /// <summary>
+        /// Generate a new map when the space key is pressed.
+        /// </summary>
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                BuildMap();
+            }
+        }
+
+        /// <summary>
+        /// Draw a debug representation of the map.
+        /// </summary>
+        private void OnDrawGizmos()
+        {
+            if (map == null)
+            {
+                return;
+            }
+
+            for (int x = 0; x < width; ++x)
+            {
+                for (int y = 0; y < height; ++y)
+                {
+                    Location location = new Location(x, y);
+
+                    Gizmos.color = (map.GetValueAtLocation(location) == 1) ? Color.black : Color.white;
+                    Vector3 pos = new Vector3(-width / 2 + x + 0.5f, -height / 2 + y + 0.5f);
+                    Gizmos.DrawCube(pos, Vector3.one);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Build the map.
+        /// </summary>
+        private void BuildMap()
+        {
+            map = new Map(width, height);
+
+            RandomizeMap();
+            SmoothMap();
+        }
+
+        /// <summary>
+        /// Generate a randomly filled map with filled / non-filled tile status.
+        /// </summary>
+        private void RandomizeMap()
+        {
+            System.Random rand = new System.Random(Time.time.GetHashCode());
+
+            // Generate the random data for each cell in the map.
+            for (int x = 0; x < map.Width; ++x)
+            {
+                for (int y = 0; y < map.Height; ++y)
+                {
+                    Location location = new Location(x, y);
+
+                    // Set the wall cells to filled.
+                    if (x == 0 || x == map.Width - 1 || y == 0 || y == map.Height - 1)
+                    {
+                        map.SetValueAtLocation(location, 1);
+                    }
+                    // Center cells have random data.
+                    else
+                    {
+                        map.SetValueAtLocation(location, (rand.Next(0, 100) < fillPercentage) ? 1 : 0);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Smooth the map based on simple cellular automata rules.
+        /// </summary>
+        private void SmoothMap()
+        {
+            
+        }
+    }
 }
