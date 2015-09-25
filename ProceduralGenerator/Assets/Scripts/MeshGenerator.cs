a32d1d87a9efb5935e744be87686a662ba7e35a0
Andrew Jones <ajon542@gmail.com>
2015-09-25 14:04:52 -0700

    Added a mesh node class to handle the vertex position and index.

--- ProceduralGenerator/Assets/Scripts/MeshGenerator.cs
+++ ProceduralGenerator/Assets/Scripts/MeshGenerator.cs
@@ -1,4 +1,7 @@
 ﻿
+using System;
+using System.Linq;
+
 namespace Dungeon
 {
     using UnityEngine;
@@ -21,17 +24,84 @@ namespace Dungeon
         /// </summary>
         private List<int> triangles;
 
+        /// <summary>
+        /// List of nodes in the mesh.
+        /// </summary>
+        private List<MeshNode> meshNodesList;
+
         private void Start()
         {
             Mesh mesh = new Mesh();
             GetComponent<MeshFilter>().mesh = mesh;
 
-            vertices = new List<Vector3> { new Vector3(0, 0, 0), new Vector3(5, 5, 0), new Vector3(5, 0, 0) };
-            triangles = new List<int> { 0, 1, 2 };
+            vertices = new List<Vector3>();
+            triangles = new List<int>();
+
+            meshNodesList = new List<MeshNode>();
+
+            meshNodesList.Add(new MeshNode(new Vector3(0, 0, 0)));
+            meshNodesList.Add(new MeshNode(new Vector3(0, 5, 0)));
+            meshNodesList.Add(new MeshNode(new Vector3(5, 5, 0)));
+            meshNodesList.Add(new MeshNode(new Vector3(5, 0, 0)));
+
+            CreateMeshFromNodes(meshNodesList);
 
             mesh.vertices = vertices.ToArray();
             mesh.triangles = triangles.ToArray();
             mesh.RecalculateNormals();
+        }
+
+        /// <summary>
+        /// Create a triangle mesh.
+        /// </summary>
+        /// <param name="a">First meshnode in the triangle.</param>
+        /// <param name="b">Second meshnode in the triangle.</param>
+        /// <param name="c">Third meshnode in the triangle.</param>
+        private void CreateTriangle(MeshNode a, MeshNode b, MeshNode c)
+        {
+            if (vertices == null || triangles == null)
+            {
+                throw new Exception("Attempting to create a mesh before initialization");
+            }
+
+            // Add the triangle indices.
+            triangles.Add(a.VertexIndex);
+            triangles.Add(b.VertexIndex);
+            triangles.Add(c.VertexIndex);
+        }
+
+        /// <summary>
+        /// Create a mesh from the list of mesh nodes.
+        /// </summary>
+        /// <param name="meshNodes">The list of mesh nodes.</param>
+        private void CreateMeshFromNodes(List<MeshNode> meshNodes)
+        {
+            AssignVertices(meshNodes);
+
+            if (meshNodes.Count >= 3)
+            {
+                CreateTriangle(meshNodes[0], meshNodes[1], meshNodes[2]);
+            }
+            if (meshNodes.Count >= 4)
+            {
+                CreateTriangle(meshNodes[0], meshNodes[2], meshNodes[3]);
+            }
+        }
+
+        /// <summary>
+        /// Assign vertices to each node with an unassigned vertex index.
+        /// </summary>
+        /// <param name="meshNodes">The list of mesh nodes.</param>
+        private void AssignVertices(List<MeshNode> meshNodes)
+        {
+            foreach(MeshNode node in meshNodes)
+            {
+                if (node.VertexIndex == -1)
+                {
+                    node.VertexIndex = vertices.Count;
+                    vertices.Add(node.Position);
+                }
+            }
         }
     }
 }
---
+++ ProceduralGenerator/Assets/Scripts/MeshNode.cs
@@ -0,0 +1,50 @@
+﻿namespace Dungeon
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Container for a vertex position and triangle index.
+    /// </summary>
+    public class MeshNode
+    {
+        /// <summary>
+        /// Gets or sets the vertex position.
+        /// </summary>
+        public Vector3 Position { get; set; }
+
+        /// <summary>
+        /// Gets or sets the vertex index for the triangles.
+        /// </summary>
+        public int VertexIndex { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="MeshNode"/> class.
+        /// </summary>
+        public MeshNode()
+        {
+            Position = new Vector3();
+            VertexIndex = -1;
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="MeshNode"/> class.
+        /// </summary>
+        /// <param name="position">The vertex position.</param>
+        public MeshNode(Vector3 position)
+        {
+            Position = position;
+            VertexIndex = -1;
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="MeshNode"/> class.
+        /// </summary>
+        /// <param name="position">The vertex position.</param>
+        /// <param name="vertexIndex">The vertex index.</param>
+        public MeshNode(Vector3 position, int vertexIndex)
+        {
+            Position = position;
+            VertexIndex = vertexIndex;
+        }
+    }
+}
